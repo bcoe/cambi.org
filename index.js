aa3610dded2039f38b934ba8776fd6fb02749a6d
Benjamin <ben@npmjs.com>
2018-01-26 18:09:07 -0800

    fix: use the PORT that heroku expects

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ app.use(express.static('public'))
 // this is where we'll add Keanan's stuff.
 // app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(3000 || process.env.PORT, () => console.log(`Cambi's celebration running on ${process.env.PORT}`))
